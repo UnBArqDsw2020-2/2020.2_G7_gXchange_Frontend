9f00bf687d590043f465a719dc9b0133f4185ca5
igor-paiva <igorbatistapaiva@outlook.com>
2021-04-04 19:42:30 -0300

    Refactor API Adapter class

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,34 +2,50 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import env from 'react-dotenv';
 
-const getConfig = () => ({
-  timeout: 5000,
-  baseURL: `${env.API_URL}/app/`,
+const CONFIG = {
+  timeout: env.GXCHANGE_TIMEOUT,
+  baseURL: `${env.GXCHANGE_API_URL}/app/`,
   headers: {
     Accept: 'application/json',
   },
-});
+};
 
 export default class APIAdapter {
   private instance: AxiosInstance;
 
   constructor() {
-    this.instance = axios.create(getConfig());
+    this.instance = axios.create(CONFIG);
   }
 
-  get(path: string, config?: AxiosRequestConfig) {
-    return this.instance.get(path, config);
+  async get(path: string, config?: AxiosRequestConfig) {
+    const res = await this.instance.get(path, config);
+
+    return res.data ? res.data : res;
   }
 
-  post(path: string, data?: Record<string, any>, config?: AxiosRequestConfig) {
-    return this.instance.post(path, data, config);
+  async post(
+    path: string,
+    data?: Record<string, any>,
+    config?: AxiosRequestConfig,
+  ) {
+    const res = await this.instance.post(path, data, config);
+
+    return res.data ? res.data : res;
   }
 
-  patch(path: string, data?: Record<string, any>, config?: AxiosRequestConfig) {
-    return this.instance.patch(path, data, config);
+  async patch(
+    path: string,
+    data?: Record<string, any>,
+    config?: AxiosRequestConfig,
+  ) {
+    const res = await this.instance.patch(path, data, config);
+
+    return res.data ? res.data : res;
   }
 
-  delete(path: string, config?: AxiosRequestConfig) {
-    return this.instance.delete(path, config);
+  async delete(path: string, config?: AxiosRequestConfig) {
+    const res = await this.instance.delete(path, config);
+
+    return res.data ? res.data : res;
   }
 }
