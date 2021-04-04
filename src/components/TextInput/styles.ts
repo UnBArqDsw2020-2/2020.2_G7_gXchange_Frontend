c399ef5369a710c34969bed279124ff7ff006385
Washington Bispo <JuniorBispo515@gmail.com>
2021-04-04 10:55:04 -0300

    Created Text-input component
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>

---
+++ src/components/TextInput/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  margin: 16px 0;
+  flex-direction: column;
+
+  span {
+    margin-bottom: 8px;
+  }
+`;
