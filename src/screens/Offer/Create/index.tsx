67cbc2decfd43fd21180a9263cd625ce1ff1546e
Thiago Lopes <thiago.lopes.santos.tls@gmail.com>
2021-04-14 21:15:25 -0300

    Validate and send the request for the offer
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>

--- src/screens/Offer/Create/index.tsx
+++ src/screens/Offer/Create/index.tsx
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import imageCompression from 'browser-image-compression';
-
 import APIAdapter from '../../../services/api';
 import { openModal } from '../../../store/GlobalModal';
-
 import GameForm, { IPicture, IGameInfo } from '../GameForm';
+import { compressImages, parsePicturesToBase64 } from '../../../utils/images';
 
 import { Container } from './styles';
 
 const isStrInvalid = (value: string | null | undefined) => !value;
-
-const COMPRESSION_OPTIONS = {
-  maxSizeMB: 0.5,
-  fileType: 'png',
-  useWebWorker: true,
-  maxWidthOrHeight: 500,
-};
 
 const CreateOffer: React.FC = () => {
   const dispatch = useDispatch();
@@ -28,11 +19,7 @@ const CreateOffer: React.FC = () => {
     try {
       setLoading(true);
 
-      const compressions = pics.map((picture) =>
-        imageCompression(picture.file, COMPRESSION_OPTIONS),
-      );
-
-      return await Promise.all(compressions);
+      return await compressImages(pics.map((picture) => picture.file));
     } catch {
       dispatch(
         openModal({
@@ -48,20 +35,86 @@ const CreateOffer: React.FC = () => {
     return [];
   };
 
+  const validateOffer = (offerData: IGameInfo) => {
+    try {
+      const { cep, type, price, gameName, pictures, plataform } = offerData;
+
+      if (isStrInvalid(gameName))
+        throw Error('Nome do jogo é um campo obrigatório.');
+      if (isStrInvalid(plataform))
+        throw Error('Plataforma é um campo obrigatório');
+      if (isStrInvalid(cep) || cep.length !== 8)
+        throw Error('O cep é obrigatório');
+      if ((type === 2 || type === 3) && price <= 0)
+        throw Error(
+          'Se o anúncio é de venda ou venda e troca, é necessário informar um preço válido',
+        );
+      if (pictures.length <= 0)
+        throw Error('O anúncio deve possuir pelo menos uma foto');
+
+      return true;
+    } catch (error) {
+      dispatch(
+        openModal({
+          title: 'Erro',
+          type: 'error',
+          content: error.message,
+        }),
+      );
+    }
+
+    return false;
+  };
+
+  const getRequestData = (offerData: IGameInfo, base64Images: string[]) => {
+    const {
+      cep,
+      type,
+      price,
+      gameName,
+      plataform,
+      condition,
+      description,
+    } = offerData;
+
+    return {
+      cep,
+      plataform,
+      condition,
+      game_name: gameName,
+      ...(price > 0 && { price }),
+      user: 'joaothebrabo@mail.com',
+      is_trade: type === 1 || type === 3,
+      ...(!!description && { description }),
+      pictures: base64Images.map((item) => ({
+        bin: item.replace('data:image/png;base64,', ''),
+      })),
+    };
+  };
+
   const createOffer = async (offerData: IGameInfo) => {
     try {
       setLoading(true);
 
       const apiAdapter = new APIAdapter();
 
+      if (!validateOffer(offerData)) return;
+
       const { pictures } = offerData;
 
-      const data = {
-        ...offerData,
-        pictures: pictures.map((item) => ({ picture: item.file })),
-      };
+      const compressedPictures = await compressPictures(pictures);
 
-      await apiAdapter.post('/offer', data);
+      const base64Images = await parsePicturesToBase64(compressedPictures);
+
+      await apiAdapter.post('/offer', getRequestData(offerData, base64Images));
+
+      dispatch(
+        openModal({
+          title: 'Sucesso',
+          type: 'success',
+          content: 'Anúncio criado com sucesso!',
+        }),
+      );
     } catch {
       dispatch(
         openModal({
