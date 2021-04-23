05883429c9cc320c2be552471d87d765b0c1735a
marcelog5 <marcelovictorg2@gmail.com>
2021-04-23 20:30:24 -0300

    Add ibge API
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

---
+++ src/services/api_ibge.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const CONFIG: AxiosRequestConfig = {
+  timeout: parseInt(process.env.REACT_APP_GXCHANGE_TIMEOUT || '5000', 10),
+  baseURL: `http://viacep.com.br/ws`,
+  headers: {
+    Accept: 'application/json',
+  },
+};
+
+export default class APIAdapter {
+  private instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create(CONFIG);
+  }
+
+  async get(path: string) {
+    const res = await this.instance.get(`${path}/json/`);
+
+    return res.data.results || res.data;
+  }
+}
