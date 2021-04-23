f69ef20fef6cf4ba4f89a746d5d0364f74103e5b
marcelog5 <marcelovictorg2@gmail.com>
2021-04-23 20:32:53 -0300

    Creat show offer page
    Co-authored-by: ThiagoGMF <thiago.guilherme20@hotmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from '../screens/Signup';
 import CreateOffer from '../screens/Offer/Create';
+import ShowOffer from '../screens/Offer/Show';
 import EditUser from '../screens/EditUser';
 
 const Routes: React.FC = () => (
@@ -9,6 +10,7 @@ const Routes: React.FC = () => (
     <Switch>
       <Route exact path="/cadastro" component={Signup} />
       <Route exact path="/oferta/cadastro" component={CreateOffer} />
+      <Route exact path="/oferta/visualizar" component={ShowOffer} />
       <Route exact path="/usuario/editar" component={EditUser} />
     </Switch>
   </Router>
